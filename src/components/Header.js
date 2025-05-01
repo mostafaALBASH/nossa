f817f52cd58eaaf38092c52dc090e07d0adfb82a
mostafaALBASH <mostafa_elbesh@outlook.com>
2025-05-01 15:37:40 +0200

    replace API urls

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
 
   const loadMenuLinksData = async() => {
     // Query the API Gateway
-    const resp = await fetch('https://a6lnno41t7.execute-api.us-east-1.amazonaws.com/Production/menu_links');
+    const resp = await fetch('https://ig37mq7zfj.execute-api.eu-north-1.amazonaws.com/Production/menu_links');
     let jsonData = await resp.json();
 
     // Assign response data to our state variable
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,7 +6,7 @@ const Welcome = () => {
 
   const loadGalleryImagesData = async() => {
     // Query the API Gateway
-    const resp = await fetch('https://a6lnno41t7.execute-api.us-east-1.amazonaws.com/Production/gallery_images');
+    const resp = await fetch('https://ig37mq7zfj.execute-api.eu-north-1.amazonaws.com/Production/gallery_images');
     let jsonData = await resp.json();
 
     // Assign response data to our state variable
